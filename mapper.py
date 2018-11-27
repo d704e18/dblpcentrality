76a1fd4207d83a206fa231e209238d303b500fa7
Magnus Nørhave Hansen <magnha14@student.aau.dk>
2018-11-27 10:56:35 +0100

    Add cite support to mapper

--- mapper.py
+++ mapper.py
@@ -4,11 +4,17 @@ import sys
 article_key = ''
 for line in sys.stdin:
     if '<article' in line:
-        key_start_index = line.find('key=') + 4
-        key_end_index = line[key_start_index:].find('>') + key_start_index
+        key_start_index = line.find('key="') + 5
+        key_end_index = line[key_start_index:].find('"') + key_start_index
         article_key = line[key_start_index:key_end_index]
     if '<author' in line:
         author_start_index = line.find('>') + 1
         author_end_index = line[author_start_index:].find('<') + author_start_index
         author = line[author_start_index:author_end_index]
-        print('{0}\t{1}'.format(article_key, author))
+        print('author\t{0}\t{1}'.format(article_key, author))
+    if '<cite' in line:
+        cite_start_index = line.find('>') + 1
+        cite_end_index = line[cite_start_index:].find('<') + cite_start_index
+        cite = line[cite_start_index:cite_end_index]
+        if cite != '...':
+            print('cite\t{0}\t{1}'.format(cite, 1))
